d093e1a86e6d0d97ecc13c4280a9f43e078d5296
mtlbaur <42392634+mtlbaur@users.noreply.github.com>
2018-10-15 19:23:31 -0400

    Added validation code for a time scale of points and intervals.

--- timescalecalculus.py
+++ timescalecalculus.py
@@ -19,6 +19,52 @@ class timescale:
     def __init__(self,ts,name='none'):
         self.ts = ts
         self.name = name
+
+        #
+        # The following code validates the user-specified timescale to ensure that there are no overlaps such as:
+        #   - a point given more than once
+        #   - a point that is included in an interval
+        #   - a point that is the starting value or ending value of an interval
+        #   - an interval given more than once
+        #   - overlapping intervals
+        #
+        # If a timescale is detected as invalid an Exception will be generated with a corresponding message that describes the cause of the invalidity.
+        #
+        for listItem in ts:
+            if isinstance(listItem, list):
+                if len(listItem) > 2:
+                    raise Exception("Invalid timescale declaration: you cannot have an interval with more than one starting and one ending value.")
+
+                if len(listItem) < 2:
+                    raise Exception("Invalid timescale declaration: an interval must have a starting value and an ending value.")
+
+                if listItem[0] > listItem[1]:
+                    raise Exception("Invalid timescale declaration: you cannot have an interval in which the ending value is smaller than the starting value.")
+
+                if listItem[0] == listItem[1]:
+                    raise Exception("Invalid timescale declaration: you cannot have an interval in which the starting value and ending value are equal (such an interval should be declared as a point).")
+
+            for listItemToCompare in ts:
+                if listItem == listItemToCompare and listItem is not listItemToCompare:
+                    raise Exception("Invalid timescale declaration: you cannot include the same point or interval more than once.")
+
+                if listItem is not listItemToCompare:
+                    if isinstance(listItem, list) and isinstance(listItemToCompare, list):
+                        if (listItem[0] >= listItemToCompare[0] and listItem[0] <= listItemToCompare[1]) or (listItem[1] >= listItemToCompare[0] and listItem[1] <= listItemToCompare[1]):
+                            raise Exception("Invalid timescale declaration: you cannot have overlapping intervals.")
+
+                    if isinstance(listItem, list) and not isinstance(listItemToCompare, list):
+                        if listItemToCompare >= listItem[0] and listItemToCompare <= listItem[1]:
+                            raise Exception("Invalid timescale declaration: you cannot declare a point that is included in an interval (you cannot declare a value more than once).")
+
+                    if not isinstance(listItem, list) and isinstance(listItemToCompare, list):
+                        if listItem >= listItemToCompare[0] and listItem <= listItemToCompare[1]:
+                            raise Exception("Invalid timescale declaration: you cannot declare a point that is included in an interval (you cannot declare a value more than once).")
+
+        print("Timescale successfully constructed:")
+        print("Timescale:", self.ts)
+        print("Timescale name:", self.name)
+
     #
     #
     # forward jump
